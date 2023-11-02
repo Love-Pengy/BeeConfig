8ad3d39969d6e8c62dc54d4150ec57c935cf77d1
Bee <afrazierboy2001@gmail.com>
2023-11-02 17:15:23 -0400

    indent colorizer fix

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,10 +1,9 @@
---fu-ction transBG(color)
---	color = color or "middlenight_blue"
---	vim.cmd.colorscheme(color)
---	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
---	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+ function transBG(color)
+	color = color or "middlenight_blue"
+	vim.cmd.colorscheme(color)
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
---end
+end
 
---this shit looks like hot ass on my other machine so its disabled
---transBG()
+transBG()
