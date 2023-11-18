d1e4a497639d09753877f0c2526a539d5c8d095b
Bee <afrazierboy2001@gmail.com>
2023-11-18 18:19:32 -0500

    SignColumn transparency addition

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -4,7 +4,7 @@
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
-
+    vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
 end
 
 transBG()
