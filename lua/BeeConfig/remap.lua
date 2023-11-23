21d57f010aa4116b34a3b8a7486b317574b9d38d
Bee <afrazierboy2001@gmail.com>
2023-11-23 18:48:08 -0500

    buffer keybinds, inverse tab, tabs in visual mode

--- lua/BeeConfig/remap.lua
+++ lua/BeeConfig/remap.lua
@@ -14,6 +14,8 @@ vim.keymap.set("n", "y", "\"+Y")
 vim.keymap.set("n", "d", "\"_d")
 vim.keymap.set("v", "d", "\"_d")
 
+
+--a little encouragement
 vim.keymap.set("n", '<Left>', ':echoe "Use h"<CR>')
 vim.keymap.set("n", '<Right>', ':echoe "Use l"<CR>')
 vim.keymap.set("n", '<Up>', ':echoe "Use k"<CR>')
@@ -22,4 +24,18 @@ vim.keymap.set("i", '<Left>' , '<ESC>:echoe "Use h"<CR>')
 vim.keymap.set("i", '<Right>' , '<ESC>:echoe "Use l"<CR>')
 vim.keymap.set("i", '<Up>' , '<ESC>:echoe "Use k"<CR>')
 vim.keymap.set("i", '<Down>' , '<ESC>:echoe "Use j"<CR>')
+
+
+--buffer (im finna use these as tabs) keybinds
+vim.keymap.set("n", '<leader>t', ':enew<CR>')
+vim.keymap.set("n", '<leader>d', ':bd<CR>')
+vim.keymap.set("n", '<leader><Tab>', ':bnext<CR>')
+vim.keymap.set("n", '<leader><S-Tab>', ':bprev<CR>')
+
+
+--inverse tab in insert mode
+vim.keymap.set("i", '<S-Tab>', '<C-d>')
+--tabs in visual mode
+vim.keymap.set("v", '<S-Tab>', '<<') 
+vim.keymap.set("v", '<Tab>', '>>') 
 
