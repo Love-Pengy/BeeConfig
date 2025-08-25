fade7ad2ef37cafcfd6dad1d5e60cb20045c9c1c
Love-Pengy <afrazierboy2001@gmail.com>
2025-08-24 21:11:07 -0400

    respect git ignore

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,7 +3,7 @@ return {
     dependencies = { "nvim-lua/plenary.nvim", "sharkdp/fd" },
     config = function()
         local builtin = require("telescope.builtin")
-        vim.keymap.set("n", "<leader>pf", "<cmd>Telescope find_files no_ignore=true<CR>", {})
-        vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
+        vim.keymap.set("n", "<leader>pf", "<cmd>Telescope find_files<CR>", {})
+        vim.keymap.set("n", "<leader>ps", "<cmd>Telescope live_grep<CR>", {})
     end,
 }
