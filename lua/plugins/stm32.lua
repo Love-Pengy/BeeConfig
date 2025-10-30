03e553ced4a079ce42a0ed670c4ff6d5322cb6b5
brandon.frazier <brandon.frazier@powertraincontrol.com>
2025-10-30 14:12:25 -0400

    swap undotree for telescope undotree

--- lua/plugins/stm32.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "alex-schulster/stm_lsp_nvim",
-    config = function()
-        require("stm_lsp_nvim").setup({
-        -- Custom configuration, or leave empty for default config
-        })
-    end
-}
