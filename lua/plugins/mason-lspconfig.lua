ed9d95f9666f9dbc5a7ef021330237aaa9e7d15f
brandon.frazier <brandon.frazier@powertraincontrol.com>
2025-11-11 14:37:17 -0500

    add markdown linter/formatter

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,8 +1,23 @@
 return {
     "mason-org/mason-lspconfig.nvim",
     opts = {
-        ensure_installed = { "lua_ls", "clangd", "pylsp",
-            "cssls", "bashls", "yamlls", "harper_ls" }
+        ensure_installed = {
+            -- Lua
+            "lua_ls",
+            -- C/Cpp
+            "clangd",
+            -- python
+            "pylsp",
+            -- html/css
+            "cssls",
+            -- bash
+            "bashls",
+            -- yaml
+            "yamlls",
+            -- comments
+            "harper_ls",
+            -- markdown
+            "marksman" }
     },
     dependencies = {
         { "mason-org/mason.nvim", opts = {} },
